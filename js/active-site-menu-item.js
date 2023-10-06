e7a0769cff5a0e4f081f90bb7ff28c3013464d44
glocalchaos <glocal.chaos@mail.ru>
2023-10-06 12:17:27 +0300

    added pages

--- js/active-site-menu-item.js
+++ js/active-site-menu-item.js
@@ -1,13 +1,50 @@
-var currentUrl = document.location.pathname;
+/* let currentUrl = document.location.pathname;
 
-var menuLinks = document.querySelectorAll('a');
+let menuLinks = Array.from(
+    document.getElementsByClassName('.site-menu-item')
+);
 
+/* ############ 
+console.log("чё смотришь " + document.location.pathname + " " + menuLinks.length)
 for (var i = 0; i < menuLinks.length; i++) {
-    var link = menuLinks[i];
+    console.log(i + " " + menuLinks[i].href)
+    let link = menuLinks[i];
 
     if (link.href === currentUrl) {
 
         link.classList.add('.active');
-        return;
+        break;
     }
-}
+} #############*/
+
+/* console.log("чё смотришь " + document.location.pathname + " " + menuLinks.length)
+menuLinks.forEach(menuLink => {
+    if (menuLink.href === currentUrl) {
+        menuLink.classList.add(active);
+    }
+}) */
+
+document.addEventListener("DOMContentLoaded", function() {
+    let currentUrl = document.location.pathname;
+    if (currentUrl.slice(-1) === "/") {
+        currentUrl = currentUrl.slice(0, -1);
+    }
+
+    let menuLinks = Array.from(
+        document.querySelectorAll('.site-menu-item')
+    );
+
+    console.log("чё смотришь " + document.location.pathname + " " + menuLinks.length)
+    
+    menuLinks.forEach(link => {
+        let linkHref = link.getAttribute("href").split("/").reverse()[0];
+
+                    console.log(link);
+
+                    
+        if (document.location.href.indexOf(linkHref) > -1) {
+            console.log("gotcha!" + document.location.href + " === " + linkHref);
+            link.classList.add('active');
+        }
+    });
+  });
