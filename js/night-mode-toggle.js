cb9fd79cd60a1e9c11a8841b66d44350431eb11e
glocalchaos <glocal.chaos@mail.ru>
2023-11-19 18:21:54 +0300

    add lab 5

--- js/night-mode-toggle.js
+++ js/night-mode-toggle.js
@@ -1,6 +1,6 @@
 function nightModeToggle() {
     const elements = document.querySelectorAll("footer a, nav, .site-menu, .container_header, header, main, footer, article, main");
-    console.log(elements)
+
     //element.classList.toggle("night-mode");
     elements.forEach((element) => {
         if (!element.classList.contains("separate-block")) {
